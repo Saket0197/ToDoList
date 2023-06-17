838fb5de7b4e5cb87d479c970a30695a84dee395
Saket <saket.s9713@gmail.com>
2023-06-17 18:54:03 +0530

    bug fixes and added media queries

--- scripts.js
+++ scripts.js
@@ -4,16 +4,33 @@ const items = document.querySelector('.items');
 const clearInput = document.querySelector('.clear-input');
 const clearList = document.querySelector('.clear-list');
 let arr = [];
+let store = {};
+let currItem = 0;
 
 function addToList(val) {
 
     let listItem = document.createElement('li');
+    listItem.currItem = currItem;
+    console.log('ind for ',val,' is ',listItem.currItem);
+    currItem++;
     listItem.classList.add('itemAdded');
+
     listItem.innerHTML = `${val}<i class="fa-solid fa-xmark"></i>`;
     items.appendChild(listItem);
     itemToBeAdded.value = "";
 
+        arr.push(val);
+        console.log('arr after adding ele', arr);
+        store.listData = arr;
+        localStorage.setItem('listData',JSON.stringify(store));
+
     listItem.querySelector('.fa-xmark').addEventListener('click',() => {
+        console.log('deleting ',val,' at ind ',listItem.currItem);
+        arr.splice(listItem.currItem,1);
+        currItem--;
+        console.log('arr after splice is ',arr);
+        store.listData = arr;
+        localStorage.setItem('listData',JSON.stringify(store));
         listItem.remove();
     });
 
@@ -26,14 +43,7 @@ function addToList(val) {
 addBtn.addEventListener('click',(event) => {
     event.preventDefault();
     if(itemToBeAdded.value) {
-
-        let store = {};
-        arr.push(itemToBeAdded.value);
-        store.listData = arr;
-        localStorage.setItem('listData',JSON.stringify(store));
-
         addToList(itemToBeAdded.value);
-
         itemToBeAdded.focus();
     }
 });
@@ -50,7 +60,22 @@ clearList.addEventListener('click',(e) => {
     for(let i =0; i < itemsAdded.length; i++) {
         itemsAdded[i].remove();
     }
+
+    currItem = 0;
+
+    if('listData' in localStorage)
+        localStorage.removeItem('listData');
 });
+
+function sub(e){
+    console.log('iv is ',itemToBeAdded.value);
+    e.preventDefault();
+    console.log('hi');
+        console.log('iv is ',itemToBeAdded.value);
+        addToList(itemToBeAdded.value);
+        
+    return false;
+}
 
 function initiate() {
     
@@ -58,6 +83,7 @@ function initiate() {
         console.log(localStorage.getItem('listData'));
         let store = JSON.parse(localStorage.getItem('listData'));
         let listArr = store.listData;
+        console.log('listArr is ',listArr);
         if(listArr.length === 0)
             return;
         for(let i=0; i < listArr.length; i++){
